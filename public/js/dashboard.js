9bb2e86a23953b63f8e657b80881d8b34770ec07
Lickydan05 <102684129+Lickydan05@users.noreply.github.com>
2023-03-07 18:47:04 +0000

    kkermit

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -6,18 +6,43 @@ const postalRegEx =
   /^[ABCEGHJ-NPRSTVXY]\d[ABCEGHJ-NPRSTV-Z][ -]?\d[ABCEGHJ-NPRSTV-Z]\d$/i;
 
 const onReset = (evt) => {
-  //TODO:: Reset the reset-able fields
+  $("#notifications").checked = true;
+  $("#eco").checked = true;
+  $("#temperature").checked = 21;
+  $("#location").checked = "L7W 4T8";
+  
+  evt.preventDefault();
 };
 
 const onSubmit = (evt) => {
   //TODO::Reset any errors before submitting
 
   //TODO:: Set notifications since it doesn't need to be validated
+  let notificationsOn = $("#notifications").checked;
+
+$("#setting_notifications").textContent = notificationsOn ? "On" : "Off";
+
 
   //TODO:: Set lighting mode with a for loop since it doesn't need to be validated
+  let lightingModeOptions = document.querySelectorAll("[name='lighting_mode']");
+
+  for (let i = 0; i < lightingModeOptions.length; i++){
+    if(lightingModeOptions[i].checked){
+      $("#setting_lighting_mode").textContent = lightingModeOptions[i].value;
+    };
+
+  }
 
   //TODO:: Validate the postal code with the Regular Expression,
   //TODO:: Display an error if not valid
+  let location = $("#setting_location").value;
+
+  if(postalRegEx.test(location)){
+    // if the postal code is valid this code will run,
+  } else{
+    // add your logic here if the postal code is not valid
+  }
+
 
   //TODO:: Validate the temperature by checking the range and if it's a number
   //TODO:: Display an error if not valid
@@ -26,7 +51,7 @@ const onSubmit = (evt) => {
 };
 
 document.addEventListener("DOMContentLoaded", () => {
-  //TODO:: Add current date
-  //TODO:: Add Reset Form listener
-  //TODO:: Add Submit Form listener
+  $("#date_display").textContent = new Date().toDateString();
+  $("#reset_form").addEventListener("reset", onReset);
+  $("#update_settings").addEventListener("click", onSubmit);
 });
--- public/js/math.js
+++ public/js/math.js
@@ -2,5 +2,53 @@
 
 const $ = (selector) => document.querySelector(selector);
 
+const division = ()=>{
+    var div1 = $("#division_1").value;
+    var div2 = $("#division_2").value;
+    var result = div1/div2;
+
+    $("#division_result").value = result.toFixed(2);
+    
+    $("#int_division_result").value = parseInt(result);
+
+    $("#floor_division_result").value = Math.floor(result);
+
+    $("#ceiling_division_result").value = Math.ceil(result);
+
+    $("#rounded_division_result").value = Math.round(result);
+};
+
+const modulo = ()=>{
+    var mod1 = $("#modulo_1").value;
+    var mod2 = $("#modulo_2").value;
+    var mod_result = mod1 % mod2
+
+    $("#modulo_result").value = mod_result
+};
+
+const forLoopIteration = ()=>{
+    let limit = $("#for_limit").value;
+    let increments = parsefloat($("#for_increment").value);
+    let direction = $("#for_increment_direction").value;
+    let counter = parsefloat($("#for_counter").value);
+
+    if (direction === "negative") {
+        increments = increments * -1
+    }; 
+
+    for (let iterator = 0; iterator < limits; iterator++){
+        counter = counter + increments;
+    };
+    
+    $("#for_result").value = counter;
+
+
+
+};
+
 //Add all calculation functions
-document.addEventListener("DOMContentLoaded", () => {});
+document.addEventListener("DOMContentLoaded", () => {
+    $("#division_btn").addEventListener("click", division);
+    $("#modulo_btn").addEventListener("click", modulo);
+    $("#for_loop_btn").addEventListener("click", forLoopIteration);
+});
